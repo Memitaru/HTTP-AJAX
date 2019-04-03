0b2cc369c1e3e0ec4664a5142a7fc8bc4ce28bf7
Memitaru <amandatwah@gmail.com>
2019-04-03 16:05:33 -0700

    Importing data with axios and displaying in app

---
+++ friends/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Friend from './Components/Friend';
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      friends:[]
+    }
+  }
+
+  componentDidMount(){
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => this.setState({friends: response.data}))
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.friends.map(friend => <Friend friend={friend} />)}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ friends/src/Components/Friend.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+let FriendContainer = styled.div`
+    border: 1px solid black;
+    margin: 5px auto;
+    padding: 10px;
+    width: 600px;
+`
+
+let Name = styled.h1`
+    font-size: 25px;
+    color: dodgerblue;
+`
+
+let Details = styled.p`
+    font-size: 16px;
+`
+
+const Friend = props => {
+    return(
+        <FriendContainer>
+            <Name>Name: {props.friend.name}</Name>
+            <Details>Age: {props.friend.age}</Details>
+            <Details>Email: {props.friend.email}</Details>
+        </FriendContainer>
+    )
+}
+
+export default Friend;
